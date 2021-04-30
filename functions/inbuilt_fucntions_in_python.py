6e7f2cff7b0d070eb2efdfd4c7a2c526772eb1f8
Kiran kumar Matam <matamkiran@users.noreply.github.com>
2021-05-01 00:00:14 +0530

    Add files via upload

---
+++ functions/inbuilt_fucntions_in_python.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  3 08:45:51 2021
+
+@author: Divya
+"""
+
+a=list((10,20,30))
+b=set()
+c=tuple()
+d=dict()
+float()
+int()
+str()
+complex()
+frozenset()
+
+len(a)
+print(sum(a))
