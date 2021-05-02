151d919e31112f5fa2069639d9d03c4684fb8e83
Kiran kumar Matam <matamkiran@users.noreply.github.com>
2021-05-02 22:38:16 +0530

    Create list_avg_ex.py

---
+++ list/list_avg_ex.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 23:28:15 2020
+
+@author: 666292
+"""
+
+# Python program to get average of a list 
+def Average(lst): 
+	return sum(lst) / len(lst) 
+
+# Driver Code 
+lst = [15, 9, 55, 41, 35, 20, 62, 49] 
+average = Average(lst) 
+
+# Printing average of the list 
+print("Average of the list =", round(average, 2)) 
