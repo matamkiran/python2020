62e2e2ae02faed9664b15bc58fd43aeda7e12f47
Kiran kumar Matam <matamkiran@users.noreply.github.com>
2021-09-23 23:15:55 +0530

    Create dictionary_add.py

---
+++ dictionary/dictionary_add.py
@@ -0,0 +1,10 @@
+
+europe = {'spain':'madrid', 'france':'paris', 
+          'germany':'berlin', 'norway':'oslo' }
+
+europe["italy"]="rome"
+
+print(europe)
+
+europe["russia"]="warsaw"
+print(europe)
