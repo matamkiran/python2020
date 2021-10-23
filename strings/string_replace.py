aa8bb64c78f223d1978e687af434de544933de21
Kiran kumar Matam <matamkiran@users.noreply.github.com>
2021-10-23 13:12:59 +0530

    Add files via upload

---
+++ strings/string_replace.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 07:31:56 2021
+
+@author: matam
+"""
+
+today="its raining cats and dogs today, today is friday , today is weekend"
+
+str1=today.replace("today","TODAY")
+
+print(str1)
---
+++ strings/string_split_ex.py
@@ -0,0 +1,16 @@
+s1="today, is monday morning,we are discussing ,string class"
+print(s1[0])    
+
+print(s1)
+
+list1=s1.split(",")
+
+print(list1)
+
+s2=" "
+
+ls=["a","b","c"]
+
+s3=s2.join(ls)
+
+print(s3)
