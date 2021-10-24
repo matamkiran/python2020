5c2e098e233ba708605b5390ddd3d2a40bb89220
Kiran kumar Matam <matamkiran@users.noreply.github.com>
2021-10-24 11:48:47 +0530

    Update inbuilt_fucntions_in_python.py

--- functions/inbuilt_fucntions_in_python.py
+++ functions/inbuilt_fucntions_in_python.py
@@ -5,7 +5,7 @@ Created on Wed Feb  3 08:45:51 2021
 @author: Divya
 """
 
-a=list((10,20,30))
+a=list((10,20,30,1,2))
 b=set()
 c=tuple()
 d=dict()
@@ -14,6 +14,10 @@ int()
 str()
 complex()
 frozenset()
+sorted(a)
+type(a)
+range(0,100,10)
+
 
 len(a)
 print(sum(a))
