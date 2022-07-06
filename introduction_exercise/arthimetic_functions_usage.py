b627ad501d4c932cc608dd87abcc19323660b1e9
Kiran kumar Matam <matamkiran@users.noreply.github.com>
2022-07-07 01:37:01 +0530

    Add files via upload

---
+++ introduction_exercise/arthimetic_functions_usage.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 07:22:35 2021
+
+@author: matam
+"""
+
+import arthimetic_functions as af
+
+af.add()
+af.substraction()
+af.multiplication()
+af.sqaure()
